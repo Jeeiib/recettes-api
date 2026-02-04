e6765d528d5996aa372c697bf21a47635b4fc17c
Jean-Baptiste <renartjeanbaptiste@gmail.com>
2026-02-04 12:14:33 +0100

    Init projet Spring Boot avec config Maven et PostgreSQL

---
+++ src/main/java/com/recettes/api/RecettesApiApplication.java
@@ -0,0 +1,13 @@
+package com.recettes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecettesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecettesApiApplication.class, args);
+    }
+
+}
