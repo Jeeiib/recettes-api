0905f2055908f0fe7fc5c0067928da3fd7d1a6f0
Jean-Baptiste <renartjeanbaptiste@gmail.com>
2026-02-06 15:51:00 +0100

    Configuration OpenAPI avec authentification JWT et CORS

---
+++ src/main/java/com/recettes/api/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.recettes.api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Recettes")
+                        .description("API REST de gestion de recettes de cuisine")
+                        .version("1.0.0")
+                )
+                .addSecurityItem(new SecurityRequirement().addList("Bearer JWT"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer JWT", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                );
+    }
+}
